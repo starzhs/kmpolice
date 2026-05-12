dc0036009174fdfa89c1e94be8754afa1d7cfad6
sstarzhevskii <sstarzhevskii@yoomoney.ru>
2026-05-12 21:23:49 +0300

    Update Homebrew formula for v0.1.3

--- packaging/homebrew/Formula/kmpolice.rb
+++ packaging/homebrew/Formula/kmpolice.rb
@@ -5,11 +5,11 @@ class Kmpolice < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/starzhs/kmpolice/releases/download/v0.1.2/kmpolice-aarch64-apple-darwin.tar.gz"
-      sha256 "d4629f33d67db455e9d75c04c67c1e5fe926e32b65443696fa0716622e62cedd"
+      url "https://github.com/starzhs/kmpolice/releases/download/v0.1.3/kmpolice-aarch64-apple-darwin.tar.gz"
+      sha256 "94d0d7cba40c0050beedc42ea5051c741cc2eb56cf3e83cedb8cdd9449d0865e"
     else
-      url "https://github.com/starzhs/kmpolice/releases/download/v0.1.2/kmpolice-x86_64-apple-darwin.tar.gz"
-      sha256 "9484391682f625288d69034e8ba30d1fcc580265542a16c95fbfd7654935517a"
+      url "https://github.com/starzhs/kmpolice/releases/download/v0.1.3/kmpolice-x86_64-apple-darwin.tar.gz"
+      sha256 "7c9bee2a442bada867f888eb904ac56763c1ad20829de17f97d96b9c056e6e89"
     end
   end
 
